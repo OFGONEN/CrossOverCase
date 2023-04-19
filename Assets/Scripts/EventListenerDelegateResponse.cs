7932932ac98641aad402778e8deee1b913107e04
ofgonen_office <ofgonen@gmail.com>
2023-04-19 19:07:46 +0300

    Implement EventListenerDelegateResponse

---
+++ Assets/Scripts/EventListenerDelegateResponse.cs
@@ -0,0 +1,31 @@
+﻿/* Created by and for usage of FF Studios (2021). */
+
+namespace FFStudio
+{
+    [ System.Serializable ]
+    public class EventListenerDelegateResponse : EventListener
+    {
+#region Fields
+        public GameEvent gameEvent;
+
+        public UnityEngine.Events.UnityAction response;
+#endregion
+
+#region API
+		public override void OnEnable()
+		{
+			gameEvent.RegisterListener( this );
+		}
+
+		public override void OnDisable()
+		{
+			gameEvent.UnregisterListener( this );
+		}
+
+		public override void OnEventRaised()
+		{
+			response();
+		}
+#endregion
+    }
+}
