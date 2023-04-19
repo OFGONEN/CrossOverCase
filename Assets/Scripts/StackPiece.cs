87e245757683d2802f067d67a4ee8db8530f96af
ofgonen_office <ofgonen@gmail.com>
2023-04-19 18:59:25 +0300

    StackPiece: Spawn as glass, wood or stone

--- Assets/Scripts/StackPiece.cs
+++ Assets/Scripts/StackPiece.cs
@@ -3,14 +3,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StackPiece : MonoBehaviour
 {
 #region Fields
-  [ Header( "Components" ) ]
-    public MeshRenderer mesh_renderer;
+    public GameSettings game_settings;
 
-    StackInfo stack_info;
+  [ Header( "Components" ) ]
+    public MeshRenderer meshRenderer;
+	public TextMeshProUGUI textRenderer_left;
+	public TextMeshProUGUI textRenderer_right;
+	public Rigidbody rigidBody;
+	public Collider collider;
+
+	StackInfo stack_info;
 #endregion
 
 #region Properties
@@ -27,10 +34,52 @@ public class StackPiece : MonoBehaviour
 		transform.eulerAngles   = rotation;
 
 		stack_info = stackInfo;
+
+        switch( stack_info.mastery )
+        {
+            case 0: SpawnAsGlass();
+				break;
+            case 1: SpawnAsWood();
+				break;
+            case 2: SpawnAsStone();
+				break;
+		}
 	}
 #endregion
 
 #region Implementation
+    void SpawnAsGlass()
+    {
+		meshRenderer.sharedMaterial = game_settings.stack_piece_glass_material;
+		textRenderer_left.text      = game_settings.stack_piece_glass_text;
+		textRenderer_right.text     = game_settings.stack_piece_glass_text;
+		textRenderer_left.color     = game_settings.stack_piece_glass_text_color;
+		textRenderer_right.color    = game_settings.stack_piece_glass_text_color;
+		collider.sharedMaterial     = game_settings.stack_piece_glass_physicMaterial;
+		rigidBody.mass              = game_settings.stack_piece_glass_mass;
+	}
+
+    void SpawnAsWood()
+    {
+		meshRenderer.sharedMaterial = game_settings.stack_piece_wood_material;
+		textRenderer_left.text      = game_settings.stack_piece_wood_text;
+		textRenderer_right.text     = game_settings.stack_piece_wood_text;
+		textRenderer_left.color     = game_settings.stack_piece_wood_text_color;
+		textRenderer_right.color    = game_settings.stack_piece_wood_text_color;
+		collider.sharedMaterial     = game_settings.stack_piece_wood_physicMaterial;
+		rigidBody.mass              = game_settings.stack_piece_wood_mass;    
+    }
+
+    void SpawnAsStone()
+    {
+		meshRenderer.sharedMaterial = game_settings.stack_piece_stone_material;
+		textRenderer_left.text      = game_settings.stack_piece_stone_text;
+		textRenderer_right.text     = game_settings.stack_piece_stone_text;
+		textRenderer_left.color     = game_settings.stack_piece_stone_text_color;
+		textRenderer_right.color    = game_settings.stack_piece_stone_text_color;
+		collider.sharedMaterial     = game_settings.stack_piece_stone_physicMaterial;
+		rigidBody.mass              = game_settings.stack_piece_stone_mass;    
+    }
 #endregion
 
 #region Editor Only
