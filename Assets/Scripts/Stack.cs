9b8677818db50582efdf0a3b0f134d9ef3305a89
ofgonen_office <ofgonen@gmail.com>
2023-04-19 17:41:04 +0300

    Create Stack Script

---
+++ Assets/Scripts/Stack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stack : MonoBehaviour
+{
+    public void CreateTheStack( List< StackInfo > stackInfoList )
+    {
+
+    }
+}
