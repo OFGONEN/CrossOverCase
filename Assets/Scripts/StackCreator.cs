70eac78081c31ab3f9b75b433cf9e9e21197a84f
ofgonen_office <ofgonen@gmail.com>
2023-04-19 17:41:56 +0300

    Create StackCreator
    - Create stack_creator scriptable object

---
+++ Assets/Scripts/StackCreator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ CreateAssetMenu( fileName = "stack_creator", menuName = "ScriptableObjects/Stack Creator" )]
+public class StackCreator : ScriptableObject 
+{
+    public GameSettings game_settings;
+    public StackInfoLoader stack_info_loader;
+    public Stack stack_prefab;
+
+    List< Stack > stack_list;
+
+    public void CreateStacks()
+    {
+		var stackInfoDictionary = stack_info_loader.StackInfoDictionary;
+
+        stack_list = new List< Stack >( stackInfoDictionary.Keys.Count );
+
+        foreach( var stackInfoPair in stackInfoDictionary )
+        {
+			var stack = GameObject.Instantiate( stack_prefab );
+			stack.CreateTheStack( stackInfoPair.Value );
+
+			stack_list.Add( stack );
+		}
+
+		PositionStacks();
+	}
+
+    public void PositionStacks()
+    {
+		var startingPoint = Vector3.left * ( stack_list.Count - 1 ) / 2f;
+
+        for( var i = 0; i < stack_list.Count; i++ )
+        {
+			stack_list[ i ].transform.position = startingPoint + game_settings.stack_offset * Vector3.right * i;
+		}
+	}
+}
