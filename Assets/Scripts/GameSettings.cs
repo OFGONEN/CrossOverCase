bcc733e4b2f8685d833810689576db39422a2b82
ofgonen_office <ofgonen@gmail.com>
2023-04-19 18:41:49 +0300

    GameSettings: Stack piece settings

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -12,4 +12,22 @@ public class GameSettings : ScriptableObject
     public float stack_piece_offset_vertical;
     public float stack_piece_offset_horizontal;
     public int stack_piece_placement_mod;
+
+  [ Header( "Stack Piece Glass Settings" ) ]
+    public Material stack_piece_glass_material;
+    public PhysicMaterial stack_piece_glass_physicMaterial;
+    public float stack_piece_glass_mass;
+    public string stack_piece_glass_text;
+
+  [ Header( "Stack Piece Wood Settings" ) ]
+    public Material stack_piece_wood_material;
+    public PhysicMaterial stack_piece_wood_physicMaterial;
+    public float stack_piece_wood_mass;
+    public string stack_piece_wood_text;
+
+  [ Header( "Stack Piece Stone Settings" ) ]
+    public Material stack_piece_stone_material;
+    public PhysicMaterial stack_piece_stone_physicMaterial;
+    public float stack_piece_stone_mass;
+    public string stack_piece_stone_text;
 }
